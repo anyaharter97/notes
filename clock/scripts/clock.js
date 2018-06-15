708e9578becd0504641d607b79421805c2caf39e
Anya Harter <aharter@redhat.com>
2018-06-15 15:24:31 -0400

    more clock updates

--- clock/scripts/clock.js
+++ clock/scripts/clock.js
@@ -12,28 +12,31 @@ var Xbase=32;
 var dots=12;
 
 function clock(){
-var time=new Date ();
-var secs=time.getSeconds();
-var sec=-1.57 + Math.PI * secs/30;
-var mins=time.getMinutes();
-var min=-1.57 + Math.PI * mins/30;
-var hr=time.getHours();
-var hrs=-1.57 + Math.PI * hr/6 + Math.PI*parseInt(time.getMinutes())/360;
-for (i=0; i < dots; ++i){
-document.getElementById("dig" + (i+1)).style.top=0-15+160*Math.sin(-0.49+dots+i/1.9).toString() + "px";
-document.getElementById("dig" + (i+1)).style.left=0-14+160*Math.cos(-0.49+dots+i/1.9).toString() + "px";
+  var time=new Date ();
+  var secs=time.getSeconds();
+  var sec=-1.57 + Math.PI * secs/30;
+  var mins=time.getMinutes();
+  var min=-1.57 + Math.PI * mins/30;
+  var hr=time.getHours();
+  var hrs=-1.57 + Math.PI * hr/6 + Math.PI*parseInt(mins)/360;
+  for (i=0; i < dots; ++i){
+    document.getElementById("dig" + (i+1)).style.top=0-15+160*Math.sin(-0.49+dots+i/1.9).toString() + "px";
+    document.getElementById("dig" + (i+1)).style.left=0-14+160*Math.cos(-0.49+dots+i/1.9).toString() + "px";
+  }
+  for (i=0; i < S.length; i++){
+    document.getElementById("sec" + (i+1)).style.top =Ypos+i*Ybase*Math.sin(sec).toString()/2.5 + "px";
+    document.getElementById("sec" + (i+1)).style.left=Xpos+i*Xbase*Math.cos(sec).toString()/2.5 + "px";
+  }
+  for (i=0; i < M.length; i++){
+    document.getElementById("min" + (i+1)).style.top =Ypos+i*Ybase*Math.sin(min).toString()/2.5 + "px";
+    document.getElementById("min" + (i+1)).style.left=Xpos+i*Xbase*Math.cos(min).toString()/2.5 + "px";
+  }
+  for (i=0; i < H.length; i++){
+    document.getElementById("hour" + (i+1)).style.top =Ypos+i*Ybase*Math.sin(hrs).toString()/2.5 + "px";
+    document.getElementById("hour" + (i+1)).style.left=Xpos+i*Xbase*Math.cos(hrs).toString()/2.5 + "px";
+  }
 }
-for (i=0; i < S.length; i++){
-document.getElementById("sec" + (i+1)).style.top =Ypos+i*Ybase*Math.sin(sec).toString()/2.5 + "px";
-document.getElementById("sec" + (i+1)).style.left=Xpos+i*Xbase*Math.cos(sec).toString()/2.5 + "px";
-}
-for (i=0; i < M.length; i++){
-document.getElementById("min" + (i+1)).style.top =Ypos+i*Ybase*Math.sin(min).toString()/2.5 + "px";
-document.getElementById("min" + (i+1)).style.left=Xpos+i*Xbase*Math.cos(min).toString()/2.5 + "px";
-}
-for (i=0; i < H.length; i++){
-document.getElementById("hour" + (i+1)).style.top =Ypos+i*Ybase*Math.sin(hrs).toString()/2.5 + "px";
-document.getElementById("hour" + (i+1)).style.left=Xpos+i*Xbase*Math.cos(hrs).toString()/2.5 + "px";
-}
-setTimeout(clock, 50);
+
+function startclock(){
+  setInterval(clock, 50);
 }
